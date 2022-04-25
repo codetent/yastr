75f486c979f34cd71f1b7ea533ec25c9c43d58a6
codetent <22278777+codetent@users.noreply.github.com>
2022-04-26 01:11:42 +0200

    Add config fmt test

---
+++ tests/config/test_yaml.py
@@ -0,0 +1,34 @@
+def test_file_empty_config(pytester):
+    pytester.makefile('.txt', testfile='')
+    pytester.makefile('.yaml', **{'test-config': ''})
+
+    run = pytester.inline_run(plugins=['yastr.plugin'])
+    report = run.getfailures()[0]
+
+    assert report.failed
+    assert report.nodeid == '.'
+    assert 'ConfigError: Invalid configuration values' in report.longreprtext
+
+
+def test_file_missing_config(pytester):
+    pytester.makefile('.txt', testfile='')
+    pytester.makefile('.yaml', **{'test-config': 'skip: true'})
+
+    run = pytester.inline_run(plugins=['yastr.plugin'])
+    report = run.getfailures()[0]
+
+    assert report.failed
+    assert report.nodeid == '.'
+    assert 'ConfigError: could not determine a constructor for the tag None' in report.longreprtext
+
+
+def test_file_wrong_config(pytester):
+    pytester.makefile('.txt', testfile='')
+    pytester.makefile('.yaml', **{'test-config': 'executable: 1\nnew: "false"}'})
+
+    run = pytester.inline_run(plugins=['yastr.plugin'])
+    report = run.getfailures()[0]
+
+    assert report.failed
+    assert report.nodeid == '.'
+    assert 'ConfigError: while parsing a block mapping' in report.longreprtext
